f1eb7ad1a3b66e56b981a4f91c1e809df806b747
ning.zhou <ning.zhou@ihandysoft.com>
2021-09-05 20:00:01 +0800

    update

--- daily_trick/replaceSpace.py
+++ daily_trick/replaceSpace.py
@@ -4,9 +4,18 @@ class Solution(object):
     def replaceSpace(self, s):
         """
 
+        在 Python，字符串都被设计成「不可变」的类型，即无法直接修改字符串的某一位字符，需要新建一个字符串实现。
+
+
         时间复杂度 O(n)
 
         空间复杂度 O(n)
 
         """
-        return s.replace(" ", "%20")
+        res = []
+        for c in s:
+            if c == ' ':
+                res.append("%20")
+            else:
+                res.append(c)
+        return "".join(res)
