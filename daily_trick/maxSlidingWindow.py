7e2ee84ead14d3da4ec662a33ea4b7299858ed2c
ning.zhou <ning.zhou@ihandysoft.com>
2021-09-21 23:42:55 +0800

    update

--- daily_trick/maxSlidingWindow.py
+++ daily_trick/maxSlidingWindow.py
@@ -25,16 +25,17 @@ class Solution(object):
         if not nums or k == 0: return []
         deque = collections.deque()
 
-        for i in range(k):
+        for i in range(k):  # 先处理第一个窗口
             while deque and deque[-1] < nums[i]:
                 deque.pop()
             deque.append(nums[i])
         res = [deque[0]]
 
         for i in range(k, len(nums)):
-            if deque[0] == nums[i - k]:  # nums[i - k] 上一个窗口的第一个元素, 如果上一个窗口的第一个元素还在deque中, 则将该元素移除。
+            if deque[0] == nums[i - k]:  # nums[i - k]是上一个窗口的第一个元素, 如果上一个窗口的第一个元素还在deque中, 则将该元素移除。
                 deque.popleft()
-            while deque and deque[-1] < nums[i]:
+            while deque and deque[-1] < nums[i]:    # 循环移除deque中比nums[i]小的元素，因为比nums[i]小不会成为窗口中的最大值，
+                # 使得deque单调递减，那么最大元素就是deque[0]
                 deque.pop()
             deque.append(nums[i])
             res.append(deque[0])
